2013973a464922916ea494cfe6d4cbe838778ace
willtreuel6 <willtreuel6@gmail.com>
2023-05-14 13:06:25 -0400

    blank slate

---
+++ trade-app-front-end/src/app/app.module.ts
@@ -0,0 +1,73 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FullCalendarModule } from '@fullcalendar/angular';
+
+
+//Prime Imports
+import {AccordionModule} from 'primeng/accordion';     //accordion and accordion tab
+import {MenuItem, PrimeIcons} from 'primeng/api';  
+import { ToolbarModule } from 'primeng/toolbar';
+import { ToastModule } from 'primeng/toast';
+import { FileUploadModule } from 'primeng/fileupload';
+import { TableModule, TableRadioButton } from 'primeng/table';
+import { RatingModule } from 'primeng/rating';
+import { Avatar, AvatarModule } from 'primeng/avatar';
+import { AvatarGroup, AvatarGroupModule } from 'primeng/avatargroup';
+import {MultiSelectModule} from 'primeng/multiselect';
+import { CarouselModule} from 'primeng/carousel';
+import { CheckboxModule } from 'primeng/checkbox';
+import {StyleClassModule} from 'primeng/styleclass';
+import {RippleModule} from 'primeng/ripple';
+import {InputTextModule} from 'primeng/inputtext';
+import {InputTextarea, InputTextareaModule} from 'primeng/inputtextarea';
+import {DataViewModule} from 'primeng/dataview';
+import {Button, ButtonModule} from 'primeng/button';
+import { DialogModule } from 'primeng/dialog';
+import { DropdownModule } from 'primeng/dropdown';
+import { RadioButtonModule } from 'primeng/radiobutton';
+import { InputNumberModule } from 'primeng/inputnumber';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+
+import {MatFormFieldModule} from '@angular/material/form-field';
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    RippleModule,
+    StyleClassModule,
+    ButtonModule,
+    ToastModule,
+    FormsModule,
+    ReactiveFormsModule,
+    TableModule,
+    DataViewModule,
+    ToolbarModule,
+    HttpClientModule,
+    DialogModule,
+    ConfirmDialogModule,
+    BrowserAnimationsModule,
+    FullCalendarModule,
+    InputTextareaModule,
+    InputTextModule,
+    ButtonModule,
+    AccordionModule,
+    AvatarGroupModule,
+    AvatarModule,
+    MatFormFieldModule,
+    CarouselModule
+    
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
