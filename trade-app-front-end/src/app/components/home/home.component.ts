8966d4bc664bc641942fd405849a342e06da1035
willtreuel6 <willtreuel6@gmail.com>
2023-05-14 21:41:07 -0400

    home changes

--- trade-app-front-end/src/app/components/home/home.component.ts
+++ trade-app-front-end/src/app/components/home/home.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import gallery from '../../data/gallery.json';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+
+
+  gallery : any = gallery;
+
+  responsiveOptions : any;
+  
+  hover : any;
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
 
 }
